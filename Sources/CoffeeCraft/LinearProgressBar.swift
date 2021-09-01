b1bc090eb0a90dba3bb40272edaf7efdebbb47cd
greyder <ygee@yearz.co>
2021-09-01 13:42:07 +0800

    change linear progress bar implementation

--- Sources/CoffeeCraft/LinearProgressBar.swift
+++ Sources/CoffeeCraft/LinearProgressBar.swift
@@ -2,8 +2,6 @@ import SwiftUI
 
 public struct LinearProgressBar: View {
     let value: CGFloat
-    let width: CGFloat
-    let height: CGFloat
     let cornerRadius: CGFloat
     let foreground: Color
     let background: Color
@@ -16,39 +14,32 @@ public struct LinearProgressBar: View {
     ///   - cornerRadius: the corner radius of the progress bar, defaults to `height/2`
     ///   - foreground: progress bar foreground color, defaults to `accentColor`
     ///   - background: progress bar background color, defaults to `gray`
-    public init(value: CGFloat, width: CGFloat, height: CGFloat = 4, cornerRadius: CGFloat? = nil, foreground: Color = Color.accentColor, background: Color = Color.gray) {
+    public init(value: CGFloat, cornerRadius: CGFloat = 4, foreground: Color = Color.accentColor, background: Color = Color.gray) {
         
         self.value = value
-        self.width = width
-        self.height = height
+        self.cornerRadius = cornerRadius
         self.foreground = foreground
         self.background = background
-        
-        if let cornerRadius = cornerRadius {
-            self.cornerRadius = cornerRadius
-        } else {
-            self.cornerRadius = height / 2
-        }
     }
     
     public var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: cornerRadius)
-                .fill(background)
-                .frame(height: height)
-            HStack {
-                RoundedRectangle(cornerRadius: 2)
-                    .fill(foreground)
-                    .frame(width: width * value, height: height)
-                Spacer()
+        GeometryReader { proxy in
+            ZStack {
+                RoundedRectangle(cornerRadius: cornerRadius)
+                    .fill(background)
+                HStack {
+                    RoundedRectangle(cornerRadius: cornerRadius)
+                        .fill(foreground)
+                        .frame(width: proxy.size.width * value, height: proxy.size.height)
+                    Spacer()
+                }
             }
         }
-        .frame(width: width)
     }
 }
 
 struct LinearProgressBar_Previews: PreviewProvider {
     static var previews: some View {
-        LinearProgressBar(value: 0.5, width: 200)
+        LinearProgressBar(value: 0.5)
     }
 }
